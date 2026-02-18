94d621b2421fb08736af2b3fdf6ea2fbaf292288
Manuel Valenzuela <manuel_valenzuela@jabil.com>
2026-02-18 15:19:22 -0600

    feat(app): Added 404 component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,32 @@ export const routes: Routes = [
     component: MainLayout,
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard',  loadComponent: () => import('./features/dashboard/dashboard').then(c => c.Dashboard) },
-      { path: 'applications', loadComponent: () => import('./features/applications/applications').then(c => c.Applications) },
-      { path: 'catalogs/sites', loadComponent: () => import('./features/catalogs/sites/sites').then(c => c.Sites) },
-      { path: 'catalogs/devs', loadComponent: () => import('./features/catalogs/owners/owners').then(c => c.Owners) },
-    ]
-  }
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard').then((c) => c.Dashboard),
+      },
+      {
+        path: 'applications',
+        loadComponent: () =>
+          import('./features/applications/applications').then((c) => c.Applications),
+      },
+      {
+        path: 'catalogs/sites',
+        loadComponent: () => import('./features/catalogs/sites/sites').then((c) => c.Sites),
+      },
+      {
+        path: 'catalogs/devs',
+        loadComponent: () => import('./features/catalogs/owners/owners').then((c) => c.Owners),
+      },
+      {
+        path: '404',
+        loadComponent: () =>
+          import('./core/components/not-found/not-found').then((c) => c.NotFound),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
 ];
